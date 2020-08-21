a41124b41c445f88d60290b33fc873286553dd73
grune97 <andrejgrunesi@gmail.com>
2020-08-21 19:42:24 +0200

    Added new environment

---
+++ .venv/lib/xml2Conf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the XML library in GNOME applications
+#
+XML2_LIBDIR="-L/home/andrej/anaconda3/envs/.venv/lib"
+XML2_LIBS="-lxml2 -L/home/andrej/anaconda3/envs/.venv/lib -lz -L/home/andrej/anaconda3/envs/.venv/lib -llzma -lpthread -L/home/andrej/anaconda3/envs/.venv/lib -L/home/andrej/anaconda3/envs/.venv/lib -licui18n -licuuc -licudata -lm "
+XML2_INCLUDEDIR="-I/home/andrej/anaconda3/envs/.venv/include/libxml2 -I/home/andrej/anaconda3/envs/.venv/include"
+MODULE_VERSION="xml2-2.9.10"
+
